58615703c881e10ced0849c4d9619f2e5c919a7e
Triet Le <triet.lq@holistics.io>
2024-05-13 14:28:46 +0700

    feat(server): add task 2 endpoint

--- project-2/models/app.py
+++ project-2/models/app.py
@@ -1,53 +1,92 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 import cv2
 import numpy as np
-from tensorflow.keras.models import load_model
+from tensorflow.keras import models
 import os
+from recommend import find_similar_images
 
 app = FastAPI()
-_FURNITURES = ['table', 'sofa', 'lamp', 'chair', 'dresser', 'bed']
+_FURNITURES = ["table", "sofa", "lamp", "chair", "dresser", "bed"]
+
 
 def _load_model(model_name: str):
-    # model_dir = "./models"
-    # model_h5_file_path = os.path.join(model_dir, model_name)
-    return load_model(model_name)
+    return models.load_model(model_name)
+
 
 def _get_image(img_path) -> np.ndarray:
     image = cv2.imread(img_path)
     return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
+
 def _classify(img_path: str) -> str:
     img = _get_image(img_path)
     # Resize the image to match your model's input size
-    img_resized = cv2.resize(img, (50, 50))  # Update img_width and img_height with your image size
-    
+    img_resized = cv2.resize(
+        img, (50, 50)
+    )  # Update img_width and img_height with your image size
+
     # Expand dimensions to create a batch dimension
     img_array = np.expand_dims(img_resized, axis=0)
-    
-    # Make predictions
-    predictions = classification_model.predict(img_array, verbose = 0)
-    predicted_class_index = np.argmax(predictions)
-    
-    return _FURNITURES[predicted_class_index-1]
 
+    # Make predictions
+    predictions = classification_model.predict(img_array, verbose=0)
+    predicted_class_index = np.argmax(predictions)
+
+    return _FURNITURES[predicted_class_index - 1]
+
+
+def _get_img_path(img_idx: int) -> str:
+    relative_path = "../Custom_Furniture_Data"
+    file_name = f"custom-img-{img_idx}.png"
+    return os.path.join(relative_path, file_name)
+
+
+def _get_encoder(model):
+    encoder = models.Sequential()
+    for layer in model.layers[:-2]:  # Exclude the last two layers (flatten and dense)
+        encoder.add(layer)
+    return encoder
+
+
+# Task 1 Model
 classification_model_name = "model_task_1_cnn_classification.h5"
 classification_model = _load_model(classification_model_name)
 
-@app.get('/')
-def index():
-    return {'message': 'Cars Recommender ML API'}
+# Task 2 Model
+recommendation_model_name = "model_task_2_cnn_classification.h5"
+recommendation_model = _load_model(recommendation_model_name)
+encoder = _get_encoder(recommendation_model)
 
-@app.get('/classify')
+
+@app.get("/", response_class=HTMLResponse)
+def index():
+    return "<h1>Welcome to the Furniture Machine Learning API</h1>"
+
+
+@app.get("/classify")
 def classify(img_idx: int) -> dict:
     # Get relative file path
-    relative_path = "../Custom_Furniture_Data"
-    file_name = f"custom-img-{img_idx}.png"
-    img_path = os.path.join(relative_path, file_name)
+    img_path = _get_img_path(img_idx)
 
     # Predict and return the result
     predicted_furniture = _classify(img_path)
-    return {'result': predicted_furniture}
+    return {"result": predicted_furniture}
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+
+@app.get("/recommend")
+def recommend(img_idx: int) -> dict:
+    img_path = _get_img_path(img_idx)
+    paths, distances = find_similar_images(recommendation_model, encoder, img_path)
+
+    # TODO: Extract data from multi-dimension array `distances`
+    #  [[27.37364001 28.79204727 29.02428353 29.03651842 29.2367386  29.3095765 29.63712217 29.74092537 29.81967957 29.8546811 ]]
+    print(paths)
+    print(distances)
+
+    return {"paths": "haha", "distances": "haha"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
---
+++ project-2/models/recommend.py
@@ -0,0 +1,93 @@
+from sklearn.neighbors import KDTree
+import pickle
+import os
+import numpy as np
+import cv2
+from scipy.stats import pearsonr
+
+furnitures = ["table", "sofa", "lamp", "chair", "dresser", "bed"]
+
+
+def get_image_and_resize(img_path):
+    img_arr = cv2.imread(img_path)
+
+    # Downsize image for faster model training. BB
+    # Normalize the image to scale 0-1 for faster training time and better performance
+    return cv2.resize(img_arr, (100, 100)) / 255.0
+
+
+def unnest_feature_vector_mapping(mapped_tuples):
+    feature_vectors, paths = zip(*mapped_tuples)
+    return list(feature_vectors), list(paths)
+
+
+def load_category_data(category):
+    vector_file = os.path.join("../vectors", f"{category}_vector.pkl")
+
+    with open(vector_file, "rb") as file:
+        data = pickle.load(file)
+
+    feature_vectors_list, paths_list = unnest_feature_vector_mapping(data)
+
+    # Build KDTree index
+    kdtree_index = KDTree(feature_vectors_list)
+    return kdtree_index, paths_list
+
+
+def get_histogram(image: np.ndarray):
+    # Convert to gray scale
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Calculate histograms
+    histogram = cv2.calcHist([gray_image], [0], None, [256], [0, 256])
+
+    return cv2.normalize(histogram, None).flatten()
+
+
+def get_correlation_coeff(input_path, neighbor_path) -> float:
+    input_img_arr = cv2.imread(input_path)
+    neighbor_img_arr = cv2.imread(neighbor_path)
+
+    input_hist = get_histogram(input_img_arr)
+    neighbor_hist = get_histogram(neighbor_img_arr)
+
+    # Get correlation
+    correlation_coefficient, _ = pearsonr(input_hist, neighbor_hist)
+    return correlation_coefficient
+
+
+# Function to find similar images for a given query image
+def find_similar_images(model, encoder, query_image_path, top_k=10):
+    # Preprocess query image
+    query_img = get_image_and_resize(query_image_path)
+
+    # get the image cat
+    predicted_category_index = model.predict(np.expand_dims(query_img, axis=0)).argmax()
+    predicted_category = furnitures[predicted_category_index - 1]
+
+    # Load corresponding vectors from the vectors folder
+    kdtree_index, img_paths = load_category_data(predicted_category)
+
+    # Encode input image to feature vector
+    query_features = encoder.predict(np.expand_dims(query_img, axis=0)).flatten()
+
+    # Perform nearest neighbor search using KDTree index
+    distances, indices = kdtree_index.query(query_features.reshape(1, -1), k=top_k)
+
+    # Return paths of similar images
+    similar_image_paths = [img_paths[idx] for idx in indices[0]]
+    print(similar_image_paths)
+    # Adhocly replace img path
+    similar_image_paths = [
+        f"../{path.replace('task1-2-furnitures', 'Furniture_Data')}"
+        for path in similar_image_paths
+    ]
+    print(similar_image_paths)
+
+    # Get correlation scores
+    # correlation_coeffs = [
+    #     get_correlation_coeff(query_image_path, neighbor_path)
+    #     for neighbor_path in similar_image_paths
+    # ]
+
+    return similar_image_paths, distances
